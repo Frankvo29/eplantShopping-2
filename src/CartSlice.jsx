2a382e030df7ef5ffa633a6905f103350f120268
Frank <khanhvo.frank@gmail.com>
2024-12-10 21:14:56 +0700

    test 1

---
+++ src/CartSlice.jsx
@@ -0,0 +1,31 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const CartSlice = createSlice({
+  name: 'cart',
+  initialState: {
+    items: [], // Initialize items as an empty array
+  },
+  reducers: {
+
+    addItem: (state, action) => {
+      const { name, image, cost} = action.payload;
+      state.items.push({name, image, cost, quantity: 1})
+    },
+
+    removeItem: (state, action) => {
+      state.items = state.items.filter(item => item.name !== action.payload.name)
+    },
+
+    updateQuantity: (state, action) => {
+      const { name, quantity } = action.payload;
+      const itemToUpdate = state.items.find(item => item.name === name);
+      if (itemToUpdate) {
+        itemToUpdate.quantity = quantity;
+      }
+    },
+  },
+});
+
+export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
+
+export default CartSlice.reducer;
